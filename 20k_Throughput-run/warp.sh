baf2718ca91499fe09b885ecb55edda7ce02a3af
Snehal Adsule <no-reply@intel.com>
2021-06-03 16:39:48 -0700

    pre-release warp 2.3.3
    Signed-off-by: Snehal Adsule <no-reply@intel.com>

---
+++ 20k_Throughput-run/warp.sh
@@ -0,0 +1,3 @@
+wget https://github.com/broadinstitute/warp/releases/download/WholeGenomeGermlineSingleSample_v2.3.3/WholeGenomeGermlineSingleSample_v2.3.3.zip
+wget https://github.com/broadinstitute/warp/releases/download/WholeGenomeGermlineSingleSample_v2.3.3/WholeGenomeGermlineSingleSample_v2.3.3.wdl
+
